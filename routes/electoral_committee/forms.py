4b305d1e2cd7b5b1913bccff348ab55e6bbbfb4d
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2024-01-22 12:52:24 -0300

    [ADD] login form class

--- routes/electoral_committee/forms.py
+++ routes/electoral_committee/forms.py
@@ -1,4 +1,7 @@
 from flask_wtf import FlaskForm
+from wtforms import PasswordField
+from wtforms.validators import InputRequired, Length
 
 class Login(FlaskForm):
-    pass
+    ci = PasswordField('CI', validators=[InputRequired(), Length(min=7, max=10)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=6, max=12)])
