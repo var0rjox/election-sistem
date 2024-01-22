277ce29431de255cd3e2310680a1664b58212c29
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2024-01-22 12:39:28 -0300

    [ADD] file structure

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from routes.home import home
 from routes.user import user
+from routes.electoral_committee.committee import electoral_committee
 
 app = Flask(__name__)
 
 app.register_blueprint(home)
 app.register_blueprint(user)
+app.register_blueprint(electoral_committee)
 
 if __name__ == "__main__":
     #app.run()
-    app.run(debug=True)
+    app.run(debug=True, host='192.168.0.7', port=9323)
---
+++ mocks/committee.py
@@ -0,0 +1,6 @@
+from models.user import User
+
+users_commission = [
+  User(12345678, 'pequeñita'),
+  User(87654321, 'pequeñito'),
+]
