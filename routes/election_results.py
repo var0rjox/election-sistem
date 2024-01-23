d357bc7c1729a8d7d98d6e1f47db0477f996cdf9
bobcodevar <vargasorellanajoseroberto@gmail.com>
2024-01-23 09:33:44 -0400

    other implementacion for href in pages routes

--- routes/election_results.py
+++ routes/election_results.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template
 
-# from models.user import User
+from models.votante import Votante , Cargo , Candidato
 
 election_results = Blueprint('election_results', __name__, url_prefix="/election_results")
 
 @election_results.route('/')
 def index():
+      # Datos de la vista
     return render_template('election_results.html')
