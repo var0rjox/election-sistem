0bab9c232c3d1392622536338da2570593760009
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2024-01-23 08:07:17 -0300

    [UPDATE] form

--- routes/electoral_committee/forms.py
+++ routes/electoral_committee/forms.py
@@ -3,6 +3,6 @@ from wtforms import PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length
 
 class Login(FlaskForm):
-    ci = PasswordField('CI', validators=[InputRequired(), Length(min=7, max=10)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=6, max=12)])
+    ci = PasswordField('CI', validators=[InputRequired(), Length(min=7, max=10)], id='ci-committee')
+    password = PasswordField('Contraseña', validators=[InputRequired(), Length(min=6, max=12)], id='password-committee')
     submit = SubmitField('Ingresar')
