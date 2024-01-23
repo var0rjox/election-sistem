0d1d1bf6f4d291d763130a4c1855977a3bdf6f3e
Emerson <veraemerson7019@gmail.com>
2024-01-22 22:57:23 -0400

    primera version de perfil de usuario

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from routes.home import home
 from routes.user import user
+from routes.elector import elector
 
 app = Flask(__name__)
 
 app.register_blueprint(home)
 app.register_blueprint(user)
+app.register_blueprint(elector)
 
 if __name__ == "__main__":
     #app.run()
---
+++ models/elector.py
@@ -0,0 +1,7 @@
+class Elector:
+    def __init__(self, ci, name, datBir, gender):
+        self.ci = ci
+        self.name = name
+        self.datBir = datBir
+        self.status = True
+        self.gender = gender
