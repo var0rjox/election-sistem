a597b2ce0072611836ad34f054307f5060b99289
Sega1A <sergio.garciacuchallo@gmail.com>
2024-01-29 08:40:56 -0400

    routes names corrected

--- routes/send_vote.py
+++ routes/send_vote.py
@@ -4,11 +4,11 @@ from controllers.vote_controller import vote_controller, send_vote_controller
 send_vote = Blueprint("send_vote", __name__)
 
 
-@send_vote.route("/send_vote")
+@send_vote.route("/enviar_voto")
 def vote():
     return send_vote_controller()
 
 
-@send_vote.route("/submit_vote", methods=["POST"])
+@send_vote.route("/subir_voto", methods=["POST"])
 def submit_vote():
     return vote_controller()
