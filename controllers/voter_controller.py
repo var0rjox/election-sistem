618158f137b07d569e2fb19088c6fb8348e3d6bf
AndrewHerrada <andrew.herrada.villarroel@gmail.com>
2024-01-31 10:46:24 -0400

    Add macro

--- controllers/voter_controller.py
+++ controllers/voter_controller.py
@@ -1,10 +1,12 @@
 from flask import session, render_template
 from services.voters_service import VotersServices
 
+
 voter_service = VotersServices()
 
 
 def voter_load_information():
     ci = session["ci"]
     current_voter = voter_service.get_single_voter(ci)
-    return render_template("voter-profile.html", voter=current_voter)
+    current_voter.type = 'voter'
+    return render_template("voter-profile.html", person=current_voter)
