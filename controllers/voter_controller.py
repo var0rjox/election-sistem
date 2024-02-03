6842170eb9d79f5cca6eec5d91bcaa236e5c77ea
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2024-02-03 18:47:05 -0300

    [FIX] enabling control

--- controllers/voter_controller.py
+++ controllers/voter_controller.py
@@ -8,5 +8,4 @@ voter_service = VotersServices()
 def voter_load_information():
     ci = session["ci"]
     current_voter = voter_service.get_single_voter(ci)
-    current_voter.type = 'voter'
     return render_template("voter-profile.html", person=current_voter)
